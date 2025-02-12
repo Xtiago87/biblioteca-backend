2eb7981599d90b2adce6e93caf1565776de7acdb
010599c4 <010599c4@sefaz-ce.gov.br>
2025-02-12 09:11:05 -0300

    - Ajuste no email do usuário para ele ser unique.

--- src/main/kotlin/com/biblioteca/model/Usuario.kt
+++ src/main/kotlin/com/biblioteca/model/Usuario.kt
@@ -1,5 +1,6 @@
 package com.biblioteca.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -11,6 +12,8 @@ data class Usuario(
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long = 0,
     val nome: String = "",
+
+    @Column(unique = true)
     val email: String = ""
 ) {
     constructor() : this(0, "", "")
